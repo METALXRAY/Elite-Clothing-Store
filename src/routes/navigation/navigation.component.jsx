6cbdd86f193f218961e8de607cffacc405c55deb
Aaryan Bhatia Ghosh <papabear.metalxray@gmail.com>
2023-02-22 13:17:37 +0530

    Added Navbar and Routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+
+import { ReactComponent as NavLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<NavLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	);
+};
+
+export default Navigation;
